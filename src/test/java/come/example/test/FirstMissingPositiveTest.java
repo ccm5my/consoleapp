447a65fad7edb703fc03d5d7e6ab8d7c0ec5c5a8
Cameron Moore <cameronmoore@Camerons-MacBook-Air.local>
2022-04-26 12:59:21 -0400

    Added tests

--- src/test/java/come/example/test/FirstMissingPositiveTest.java
+++ src/test/java/come/example/test/FirstMissingPositiveTest.java
@@ -8,7 +8,25 @@ import org.junit.Test;
 public class FirstMissingPositiveTest {
     @Test
     public void testFirstMissingPositive() {
-        int res = FirstMissingPositive.firstMissingPositive(new int[2]);
-        Assert.assertEquals(0, res);
+        int res = FirstMissingPositive.firstMissingPositive(new int[] {
+                1,2,0
+        });
+        Assert.assertEquals(3, res);
+    }
+
+    @Test
+    public void testFirstMissingPositive2() {
+        int res = FirstMissingPositive.firstMissingPositive(new int[] {
+                3,4,-1,1
+        });
+        Assert.assertEquals(2, res);
+    }
+
+    @Test
+    public void testFirstMissingPositive3() {
+        int res = FirstMissingPositive.firstMissingPositive(new int[] {
+                7,8,9,11,12
+        });
+        Assert.assertEquals(1, res);
     }
 }
